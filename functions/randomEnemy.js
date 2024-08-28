a2b829e3b14d994179f18d5131e08af91ff9c158
Khambazarov <khambazarovrenat@gmail.com>
2024-08-28 12:00:44 +0200

    jsdoc for func - randomEnemy.js

--- functions/randomEnemy.js
+++ functions/randomEnemy.js
@@ -1,4 +1,10 @@
-export const randomEnemy = (enemie) => {
-  const numberOfEnemie = Math.floor(Math.random() * enemie.length);
-  return enemie[numberOfEnemie];
+/**
+ * Wählt zufällig einen Feind aus einer gegebenen Liste von Feinden aus.
+ *
+ * @param {Array} enemy - Eine Liste von Feinden.
+ * @returns {Object} - Ein zufällig ausgewählter Feind aus der Liste.
+ */
+export const randomEnemy = (enemy) => {
+  const numberOfEnemy = Math.floor(Math.random() * enemy.length);
+  return enemy[numberOfEnemy];
 };
